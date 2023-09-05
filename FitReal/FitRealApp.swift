391fe98c96057e5637aa74d39fd1721bc7b4c7c0
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-09-06 00:44:19 +0800

    Initial Commit

---
+++ FitReal/FitRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitRealApp.swift
+//  FitReal
+//
+//  Created by Prakhar Trivedi on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FitRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
